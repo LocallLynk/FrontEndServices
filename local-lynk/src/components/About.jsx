8edb4f73392d171a479350655c96a999b2771e99
meganarmas <meganarmas@gmail.com>
2024-11-01 14:12:06 -0500

    worked on components

---
+++ local-lynk/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function AboutPage() {
+  return (
+    <div>
+      <h1>About LocalLynk</h1>
+      <h3>Welcome to LocalLynk!</h3>
+      <p>
+        LocalLynk is a project created by four Coding Temple alumni for their
+        final project. Designed with the purpose to connect others with
+        contractors in their area, we understand community is the forefront to
+        the building blocks of the world.
+      </p>
+      <p>Under construction</p>
+    </div>
+  );
+}
+
+export default AboutPage;
---
+++ local-lynk/src/components/NavBar.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { Navbar } from "react-bootstrap";
+
+function NavigationBar() {
+  return (
+    <Navbar>
+      <Navbar.Brand as={Link} to="/home">
+        Home
+      </Navbar.Brand>{" "}
+      <br />
+      <Navbar.Brand as={Link} to="/login">
+        Login
+      </Navbar.Brand>{" "}
+      <br />
+      <Navbar.Brand as={Link} to="/settings">
+        Settings
+      </Navbar.Brand>{" "}
+      <br />
+      <Navbar.Brand as={Link} to="/profile">
+        Profile
+      </Navbar.Brand>{" "}
+      <br />
+      <Navbar.Brand as={Link} to="/about-local-lynk">
+        About
+      </Navbar.Brand>{" "}
+      <br />
+    </Navbar>
+  );
+}
+
+export default NavigationBar;
