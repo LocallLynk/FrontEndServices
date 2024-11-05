b595aa7c98e6e09ec9294e710e2399daf3faa7b2
Megan <meganarmas@gmail.com>
2024-11-05 10:34:32 -0600

    Update UserContext.jsx

--- local-lynk/src/components/UserContext.jsx
+++ local-lynk/src/components/UserContext.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState, useContext } from 'react';
 
 const UserContext = React.createContext({
-    user: {name: '', isLoggedIn: false},
+    user: { name: '', isLoggedIn: false },
     setUser: () => {}
 });
+
+export const useUser = () => {
+    return useContext(UserContext);
+};
+
+export const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState({ name: '', isLoggedIn: false });
+
+    return (
+        <UserContext.Provider value={{ user, setUser }}>
+            {children}
+        </UserContext.Provider>
+    );
+};
 
 export default UserContext;
