adc00ac72e8c5108d63a3f77c47d5e41e66f5fd0
Megan <meganarmas@gmail.com>
2024-11-05 10:34:07 -0600

    Update Login.jsx

--- local-lynk/src/components/Login.jsx
+++ local-lynk/src/components/Login.jsx
@@ -1,21 +1,21 @@
 import React, { useState, useContext, useEffect } from 'react';
-import UserContext from './UserContext';
 import { useNavigate, NavLink } from 'react-router-dom';
 import { Container, Row, Col, Form, Button, Alert } from 'react-bootstrap';
+import UserContext from './UserContext';  
 
 function LoginPage() {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-    const { setUser } = useContext(UserContext);
-    const navigate = useNavigate();
     const [errorMessage, setErrorMessage] = useState('');
+    const { setUser } = useContext(UserContext);  
+    const navigate = useNavigate();
 
+    
     useEffect(() => {
         const storedUser = sessionStorage.getItem('userSession');
         if (storedUser) {
             const userSession = JSON.parse(storedUser);
-            setUser(userSession);
-
+            setUser(userSession);  
             if (userSession.name.toLowerCase() === 'admin') {
                 navigate('/feed');
             } else {
@@ -24,38 +24,33 @@ function LoginPage() {
         }
     }, [navigate, setUser]);
 
+    
     const handleLogin = (e) => {
         e.preventDefault();
         
-        // Simulate a stored user for demo purposes
+        
         const storedUser = sessionStorage.getItem('userSession');
         const userSession = storedUser ? JSON.parse(storedUser) : null;
 
         if (userSession) {
             if (userSession.name === username && userSession.password === password) {
-                // Successful login
                 const token = 'dummyToken'; // Simulate a token
                 const authenticatedUser = {
                     ...userSession,
                     token: token
                 };
+                sessionStorage.setItem('userSession', JSON.stringify(authenticatedUser));  // Save authenticated user in sessionStorage
+                setUser(authenticatedUser); 
 
-                sessionStorage.setItem('userSession', JSON.stringify(authenticatedUser));
-
-                setUser(authenticatedUser);
-
-                // Redirect based on user role
                 if (username.toLowerCase() === 'admin') {
                     navigate('/feed');
                 } else {
                     navigate('/home');
                 }
             } else {
-                // Invalid username or password
                 setErrorMessage('Invalid username or password!');
             }
         } else {
-            // No session data found
             setErrorMessage('No user data found, please register.');
         }
     };
